62d5a0a4b91606b65e48f9dd36a29e9610ee8bcd
niewinny <the.tecc@gmail.com>
2025-12-30 15:06:49 +0100

    feat: cleaup

--- __init__.py
+++ __init__.py
@@ -1,6 +1,4 @@
 """
-Copyright (C) 2015-2024 Team C All Rights Reserved
-
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
